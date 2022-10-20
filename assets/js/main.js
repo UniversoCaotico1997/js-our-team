4fdde63a3ff76ef68d1d30eb8188ca381df7dc00
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-20 15:45:32 +0200

    creazione array con all'interno gli oggetti

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1: Trasformare la stringa foto in una immagine effettiva
+// BONUS 2: Organizzare i singoli membri in card/schede
+// Consigli del giorno: Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+// E ricordiamoci che console.log() è nostro amico!
+
+
+
+// creazione di un Array di oggeti 
+    // creare un array 
+        //inserire al suo interno degli oggetti
+
+const members = [
+    {
+       nome:`Wayne Barnett`,
+       ruolo:`Founder & CEO`,
+       foto:`wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        nome:`Wayne Barnett`,
+        ruolo:`Founder & CEO`,
+        foto:`wayne-barnett-founder-ceo.jpg`
+     },
+     {
+        nome:`Wayne Barnett`,
+        ruolo:`Founder & CEO`,
+        foto:`wayne-barnett-founder-ceo.jpg`
+     },
+     {
+        nome:`Wayne Barnett`,
+        ruolo:`Founder & CEO`,
+        foto:`wayne-barnett-founder-ceo.jpg`
+     },
+     {
+        nome:`Wayne Barnett`,
+        ruolo:`Founder & CEO`,
+        foto:`wayne-barnett-founder-ceo.jpg`
+     },
+     {
+        nome:`Wayne Barnett`,
+        ruolo:`Founder & CEO`,
+        foto:`wayne-barnett-founder-ceo.jpg`
+     },
+
+]
+console.log(members);
